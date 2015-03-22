e5e69cd21914ced1483d80a83550f4a8e125cfc0
Luca de Marinis <loop23@gmail.com>
2015-03-22 12:43:18 +0100

    Load keyboard

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,7 +1,6 @@
 var spara = new Spara();
 var vista = new Vista(spara, $('div#main'));
 
-
 var holder = $('body')[0],
     state = $('#status')[0];
 
@@ -9,7 +8,7 @@ if (typeof window.FileReader === 'undefined') {
   state.className = 'fail';
 } else {
   state.className = 'success';
-  state.innerHTML = 'File API & FileReader available';
+  state.innerHTML = 'File API & FileReader available, drag text files to change what is being shot';
 }
 
 holder.ondragover = function () { this.className = 'hover'; return false; };
@@ -22,9 +21,16 @@ holder.ondrop = function (e) {
       reader = new FileReader();
   reader.onload = function (event) {
     spara.setContent(event.target.result);
+    state.innerHTML = '';
     // holder.style.background = 'url(' + event.target.result + ') no-repeat center';
   };
   console.log(file);
   reader.readAsText(file,"UTF-8");
   return false;
+};
+
+for(var i = '0'; i < '9'; i++) {
+  KeyboardJS.on(i, function() {
+    console.log("you pressed %o", i);
+  });
 };
