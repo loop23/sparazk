19e0c7f39e8f6db1c8c4e262f0b800c11b85c4cb
Luca de Marinis <loop23@gmail.com>
2015-03-28 16:12:41 +0100

    Done start/stop and help

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,9 +1,12 @@
-var spara = new Spara();
-var vista = new Vista(spara, $('div#main'));
+console.log("Starting main");
+var SparaConcetti = {};
+SparaConcetti.spara = new Spara();
+console.log("spara setup");
+SparaConcetti.vista = new Vista(SparaConcetti.spara, $('div#main'));
+console.log("vista setup");
 
 var holder = $('body')[0],
     state = $('#status')[0];
-
 if (typeof window.FileReader === 'undefined') {
   state.className = 'fail';
 } else {
@@ -16,33 +19,45 @@ holder.ondragend = function () { this.className = ''; return false; };
 holder.ondrop = function (e) {
   this.className = '';
   e.preventDefault();
-  var file = e.dataTransfer.files[0],
-      reader = new FileReader();
-  reader.onload = function (event) {
-    spara.setContent(event.target.result);
-    // state.innerHTML = '';
-    // holder.style.background = 'url(' + event.target.result + ') no-repeat center';
+  for (var i = 0; i < e.dataTransfer.files.length; i++) {
+    var file = e.dataTransfer.files[i],
+        reader = new FileReader();
+    reader.onload = function (event) {
+      SparaConcetti.spara.setContent(event.target.result, i);
+      // state.innerHTML = '';
+      // holder.style.background = 'url(' + event.target.result + ') no-repeat center';
+    };
+    console.log(file);
+    reader.readAsText(file,"UTF-8");
+    return false;
   };
-  console.log(file);
-  reader.readAsText(file,"UTF-8");
-  return false;
 };
 
 for (var i = 0; i < 10; i++) {
-  setK(i);
+  setBufKey(i);
 };
 
-function setK(key) {
+function setBufKey(key) {
+  console.log("Setting %o", key);
   KeyboardJS.on(key.toString(), function() {
     console.log("Setting buffer %i", key);
-    spara.setBuffer(key);
+    SparaConcetti.spara.setBuffer(key);
   });
 }
 
+KeyboardJS.on('h', function() {
+  $('#help').toggle();
+});
+
+KeyboardJS.on('s', function() {
+  SparaConcetti.vista.toggleSpara();
+});
+
+// Reposition text in center
 $(function() {
   $(window).on('resize', function resize()  {
     $(window).off('resize', resize);
-    setTimeout(function () {
+    window.setTimeout(function () {
       var content = $('#main');
       var top = (window.innerHeight - content.height()) / 2;
       content.css('top', Math.max(0, top) + 'px');
@@ -50,3 +65,4 @@ $(function() {
     }, 50);
   }).resize();
 });
+console.log("Done with main!");
--- public/javascripts/vista.js
+++ public/javascripts/vista.js
@@ -8,5 +8,15 @@ var Vista = function(spara, dom_id) {
     show = !show;
   }.bind(this);
   this.timerId = window.setInterval(this.tick, 500);
+  this.toggleSpara = function() {
+    if (this.timerId) {
+      console.log("Was running");
+      window.clearInterval(this.timerId);
+      this.timerId = undefined;
+    } else {
+      console.log("Was stopped");
+      this.timerId = window.setInterval(this.tick, 500);
+    }
+  };
   console.log("Initialized, timer: %i, dom_id: %o", this.timerId, dom_id);
 };
