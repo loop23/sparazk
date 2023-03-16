f86569050d2c8f1cdb91124457dff1fd8d1a4eba
Luca de Marinis <loop23@gmail.com>
2023-03-16 14:51:48 +0100

    eslint suggested fixes pt1

--- app.js
+++ app.js
@@ -1,30 +1,33 @@
 // Entry point for express app. This is the stuff that runs on the server.
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 // Mongolism
-var mongo = require('mongodb');
-var monk = require('monk');
-var db = monk(process.env.MONGODB_URI);
+// const mongo = require('mongodb');
+const monk = require('monk');
+
+const db = monk(process.env.MONGODB_URI);
 db.then(() => {
-  console.log('Connected correctly to mongodb server: %o',
-              process.env.MONGODB_URI);
+  console.log(
+    'Connected correctly to mongodb server: %o',
+    process.env.MONGODB_URI,
+  );
 });
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+const routes = require('./routes/index');
+const users = require('./routes/users');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(favicon(path.join(__dirname, '/public/favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -32,17 +35,17 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Make our db accessible to our router
-app.use(function(req,res,next){
-    req.db = db;
-    next();
+app.use((req, res, next) => {
+  req.db = db;
+  next();
 });
 
 app.use('/', routes);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
@@ -52,25 +55,24 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
+  app.use((err, req, res, _next) => {
     res.status(err.status || 500);
     res.render('error', {
       message: err.message,
-      error: err
+      error: err,
     });
   });
 }
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
+app.use((err, req, res, _next) => {
   res.status(err.status || 500);
   res.render('error', {
     message: err.message,
-    error: {}
+    error: {},
   });
 });
 
-
 module.exports = app;
-console.log("Done with app");
+console.log('Done with app');
--- routes/index.js
+++ routes/index.js
@@ -1,55 +1,56 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const crypto = require('crypto');
+const urlsafe = require('node-base64-urlsafe');
+
+const router = express.Router();
 
 /** Sync */
 function randomStringAsBase64Url(size) {
-  var crypto = require('crypto');
-  return require('node-base64-urlsafe').encode(crypto.randomBytes(size));
+  return urlsafe.encode(crypto.randomBytes(size));
 }
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  var newroom = randomStringAsBase64Url(20);
-  //var newroom = 666;
+router.get('/', (req, res) => {
+  const newroom = urlsafe.randomStringAsBase64Url(20);
+  // var newroom = 666;
   // res.redirect('/sparasimple?room=' + newroom);
-  res.redirect('/spararoom?help=true&room=' + newroom);
+  res.redirect(`/spararoom?help=true&room=${newroom}`);
 });
 
-router.get('/sparasimple*', function(req, res, next) {
-  var room = req.query.room;
-  res.render('simple',
-             {
-               'title': 'SparaConcetti Rave Edition - room:' + room,
-               'room': room
-             });
+router.get('/sparasimple*', ({ req: { query: { room } } }, res, next) => {
+  res.render('simple', {
+    title: `SparaConcetti Rave Edition - room: ${room}`,
+    room: room,
+  });
 });
 
-router.post('/', function(req, res, next) {
-  console.log("Geezer logged in from fb");
-  var room = req.query.room;
+router.post('/', ({ req: { query: { room, help } } }, res) => {
+  // console.log('Geezer logged in from fb');
   if (room) {
-    var hidehelp = req.query.help != 'true';
-    console.log("Got room: ", room, "rendering, hidehelp: " + hidehelp);
-    res.render('index', { 'title': 'SparaConcetti Rave Edition - room:' + room,
-                          'room': room,
-                          'hidehelp': hidehelp });
+    const hidehelp = help !== 'true';
+    console.log('Got room: %o, rendering, hidehelp: %o', room, hidehelp);
+    res.render('index', {
+      title: `SparaConcetti Rave Edition - room: ${room}`,
+      room,
+      hidehelp,
+    });
   } else {
     res.redirect('/');
   }
 });
 
-router.get('/spararoom*', function(req, res, next) {
-  var room = req.query.room;
+router.get('/spararoom*', ({ req: { query: { room, help } } }, res) => {
   if (room) {
-    var hidehelp = req.query.help != 'true';
-    console.log("Got room: ", room, "rendering, hidehelp: " + hidehelp);
-    res.render('index', { 'title': 'SparaConcetti Rave Edition - room:' + room,
-                          'room': room,
-                          'hidehelp': hidehelp });
+    const hidehelp = help !== 'true';
+    console.log('Got room: %o, rendering, hidehelp: %o', room, hidehelp);
+    res.render('index', {
+      title: `SparaConcetti Rave Edition - room: ${room}`,
+      room,
+      hidehelp,
+    });
   } else {
     res.redirect('/');
   }
-
 });
 
 /* GET Userlist page. */
@@ -59,21 +60,25 @@ router.get('/userlist', function(req, res) {
   var collection = db.get('usercollection');
   collection.find({},{},function(e,docs){
     res.render('userlist', {
-      "userlist" : docs
+      'userlist' : docs
     });
   });
 });
 */
-router.get("/getRoomContent", function(req, res) {
-  var room = req.query.room;
-  var db = req.db;
-  var collection = db.get("buffers");
-  console.log("Querying in room %o", room);
-  collection.find({room: room }).then((docs) => {
-    console.log("Got some docs:", docs.length);
+router.get('/getRoomContent', ({
+  req: {
+    query: {
+      room, db
+    }
+  }
+}, res) => {
+  const collection = db.get('buffers');
+  // console.log('Querying in room %o', room);
+  collection.find({ room }).then((docs) => {
+    // console.log('Got some docs: %o', docs.length);
     res.send(docs);
   }).catch((err) => {
-    console.log("Error getting room content: %o", err);
+    // console.log('Error getting room content: %o', err);
   });
 });
 
@@ -84,60 +89,61 @@ router.get('/newuser', function(req, res) {
 });
 */
 // Questa e' usata dalla simple. Non cancella
-router.post('/sendOne', function(req, res) {
-  var db = req.db;
-  var room = req.body.room || 666;
-  var text = req.body.text;
-  console.log("Inserisco %o in %o", text, room);
-  var collection  = db.get('buffers');
+router.post('/sendOne', (req, res) => {
+  const db = req.db;
+  const room = req.body.room || 666;
+  const text = req.body.text;
+  // console.log('Inserisco %o in %o', text, room);
+  const collection = db.get('buffers');
   collection.insert({
-    room: room,
+    room,
     num: 0,
-    content: text
+    content: text,
   }).then((docs) => {
-    res.render('simple',
-               {
-                 'title': 'SparaConcetti Rave Edition - room:' + room,
-                 'room': room,
-                 'message': 'Frase registrata, pronti a sparare!'
-               });
+    res.render('simple', {
+      title: `SparaConcetti Rave Edition - room: ${room}`,
+      room,
+      message: 'Frase registrata, pronti a sparare!',
+    });
   }).catch((err) => {
-    console.log("wooops - %o", err);
+    console.log('wooops - %o', err);
     res.status(500).send({
-      error: 'Non sono riuscito ad inserire il testo.. sorry!'
+      error: 'Non sono riuscito ad inserire il testo.. sorry!',
     });
   });
 });
 
 // Questa e' invocata dalla console principale; no simple
-router.post('/setBuffer', function(req, res) {
-  var db = req.db;
-  var bufnum = req.body.bufnum;
-  var room = req.body.room;
-  var content = req.body.content;
-  var collection  = db.get('buffers');
+router.post('/setBuffer', (req, res) => {
+  const db = req.db;
+  const bufnum = req.body.bufnum;
+  const room = req.body.room;
+  const content = req.body.content;
+  const collection = db.get('buffers');
   // Se c'e' gia', spianalo
-  console.log("Rimuovo in %o, num: %o", room, bufnum);
-  collection.remove({ room: room,
-                      num: bufnum }).then((docs) => {
-                        console.log("rmoved! %o", docs.deletedCount);
-                      }).catch((err) => {
-                        console.log("Can't remove docs, err: %o", err);
-                      });
-  collection.insert({
-    room: room,
+  console.log('Rimuovo in %o, num: %o', room, bufnum);
+  collection.remove({
+    room,
     num: bufnum,
-    content: content
+  }).then((docs) => {
+    console.log('rmoved! %o', docs.deletedCount);
+  }).catch((err) => {
+    console.log('Cannot remove docs, err: %o', err);
+  });
+  collection.insert({
+    room,
+    num: bufnum,
+    content,
   }).then((docs) => {
     // docs contains the documents inserted with added **_id** fields;
     // not that we really use it!
     res.status(201).end();
   }).catch((err) => {
     // An error happened while inserting
-    console.log("Cannot write to db: %o", err);
+    console.log('Cannot write to db: %o', err);
     res.status(500).send({
-      error: "There was a problem persisting data to the database",
-      exception: err
+      error: 'There was a problem persisting data to the database',
+      exception: err,
     });
   });
 });
@@ -147,24 +153,24 @@ router.post('/setBuffer', function(req, res) {
 router.post('/adduser', function(req, res) {
     // Set our internal DB variable
     var db = req.db;
-    // Get our form values. These rely on the "name" attributes
+    // Get our form values. These rely on the 'name' attributes
     var userName = req.body.username;
     var userEmail = req.body.useremail;
     // Set our collection
     var collection = db.get('usercollection');
     // Submit to the DB
     collection.insert({
-        "username" : userName,
-        "email" : userEmail
+        'username' : userName,
+        'email' : userEmail
     }, function (err, doc) {
         if (err) {
             // If it failed, return error
-            res.send("There was a problem adding the information to the database.");
+            res.send('There was a problem adding the information to the database.');
         } else {
             // If it worked, set the header so the address bar doesn't still say /adduser
-            res.location("userlist");
+            res.location('userlist');
             // And forward to success page
-            res.redirect("userlist");
+            res.redirect('userlist');
         }
     });
 });
