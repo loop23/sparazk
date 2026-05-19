1d85670c05ea59ffc83eb2dd001a720c8bc7d4d0
loop <loop@MacBook-Air-di-loop.local>
2026-05-19 12:21:11 +0200

    PWAish ma senza sw che fa casino

---
+++ app.express.js
@@ -0,0 +1,78 @@
+// Entry point for express app. This is the stuff that runs on the server.
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+// Mongolism
+// const mongo = require('mongodb');
+const monk = require('monk');
+
+const db = monk(process.env.MONGODB_URI);
+db.then(() => {
+  console.log(
+    'Connected correctly to mongodb server: %o',
+    process.env.MONGODB_URI,
+  );
+});
+
+const routes = require('./routes/index');
+const users = require('./routes/users');
+
+const app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+// uncomment after placing your favicon in /public
+app.use(favicon(path.join(__dirname, '/public/favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Make our db accessible to our router
+app.use((req, res, next) => {
+  req.db = db;
+  next();
+});
+
+app.use('/', routes);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use((err, req, res, _next) => {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err,
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use((err, req, res, _next) => {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {},
+  });
+});
+
+module.exports = app;
+console.log('Done with app');
--- app.js
+++ app.js
@@ -1,78 +1,122 @@
-// Entry point for express app. This is the stuff that runs on the server.
-const express = require('express');
-const path = require('path');
-const favicon = require('serve-favicon');
-const logger = require('morgan');
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
-// Mongolism
-// const mongo = require('mongodb');
-const monk = require('monk');
+// create constants for the form and the form controls
 
-const db = monk(process.env.MONGODB_URI);
-db.then(() => {
-  console.log(
-    'Connected correctly to mongodb server: %o',
-    process.env.MONGODB_URI,
-  );
+const newPeriodFormEl = document.getElementsByTagName("form")[0];
+const startDateInputEl = document.getElementById("start-date");
+const endDateInputEl = document.getElementById("end-date");
+const pastPeriodContainer = document.getElementById("past-periods");
+
+// Listen to form submissions.
+newPeriodFormEl.addEventListener("submit", (event) => {
+  // Prevent the form from submitting to the server
+  // since everything is client-side.
+  event.preventDefault();
+
+  // Get the start and end dates from the form.
+  const startDate = startDateInputEl.value;
+  const endDate = endDateInputEl.value;
+
+  // Check if the dates are invalid
+  if (checkDatesInvalid(startDate, endDate)) {
+    // If the dates are invalid, exit.
+    return;
+  }
+
+  // Store the new period in our client-side storage.
+  storeNewPeriod(startDate, endDate);
+
+  // Refresh the UI.
+  renderPastPeriods();
+
+  // Reset the form.
+  newPeriodFormEl.reset();
 });
 
-const routes = require('./routes/index');
-const users = require('./routes/users');
 
-const app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
-// uncomment after placing your favicon in /public
-app.use(favicon(path.join(__dirname, '/public/favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-// Make our db accessible to our router
-app.use((req, res, next) => {
-  req.db = db;
-  next();
-});
-
-app.use('/', routes);
-app.use('/users', users);
-
-// catch 404 and forward to error handler
-app.use((req, res, next) => {
-  const err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use((err, req, res, _next) => {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err,
-    });
-  });
+function checkDatesInvalid(startDate, endDate) {
+  // Check that end date is after start date and neither is null.
+  if (!startDate || !endDate || startDate > endDate) {
+    // To make the validation robust we could:
+    // 1. add error messaging based on error type
+    // 2. Alert assistive technology users about the error
+    // 3. move focus to the error location
+    // instead, for now, we clear the dates if either
+    // or both are invalid
+    newPeriodFormEl.reset();
+    // as dates are invalid, we return true
+    return true;
+  }
+  // else
+  return false;
 }
 
-// production error handler
-// no stacktraces leaked to user
-app.use((err, req, res, _next) => {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {},
-  });
-});
+// Add the storage key as an app-wide constant
+const STORAGE_KEY = "period-tracker";
 
-module.exports = app;
-console.log('Done with app');
+function storeNewPeriod(startDate, endDate) {
+  // Get data from storage.
+  const periods = getAllStoredPeriods();
+
+  // Add the new period object to the end of the array of period objects.
+  periods.push({ startDate, endDate });
+
+  // Sort the array so that periods are ordered by start date, from newest
+  // to oldest.
+  periods.sort((a, b) => new Date(b.startDate) - new Date(a.startDate));
+
+  // Store the updated array back in the storage.
+  window.localStorage.setItem(STORAGE_KEY, JSON.stringify(periods));
+}
+
+function getAllStoredPeriods() {
+  // Get the string of period data from localStorage
+  const data = window.localStorage.getItem(STORAGE_KEY);
+
+  // If no periods were stored, default to an empty array
+  // otherwise, return the stored data as parsed JSON
+  const periods = data ? JSON.parse(data) : [];
+
+  return periods;
+}
+
+function renderPastPeriods() {
+  // get the parsed string of periods, or an empty array.
+  const periods = getAllStoredPeriods();
+
+  // exit if there are no periods
+  if (periods.length === 0) {
+    return;
+  }
+
+  // Clear the list of past periods, since we're going to re-render it.
+  pastPeriodContainer.textContent = "";
+
+  const pastPeriodHeader = document.createElement("h2");
+  pastPeriodHeader.textContent = "Past periods";
+
+  const pastPeriodList = document.createElement("ul");
+
+  // Loop over all periods and render them.
+  periods.forEach((period) => {
+    const periodEl = document.createElement("li");
+    periodEl.textContent = `From ${formatDate(
+      period.startDate,
+    )} to ${formatDate(period.endDate)}`;
+    pastPeriodList.appendChild(periodEl);
+  });
+
+  pastPeriodContainer.appendChild(pastPeriodHeader);
+  pastPeriodContainer.appendChild(pastPeriodList);
+}
+
+function formatDate(dateString) {
+  // Convert the date string to a Date object.
+  const date = new Date(dateString);
+
+  // Format the date into a locale-specific string.
+  // include your locale for better user experience
+  return date.toLocaleDateString("en-US", { timeZone: "UTC" });
+}
+
+
+renderPastPeriods();
+
